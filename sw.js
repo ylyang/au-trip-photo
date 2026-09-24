7e00c5c94e26d872f6cecb573eab30b3b86b871e
Yiling Yang <csylyang@gmail.com>
2026-09-24 18:05:44 +0800

    style: brighten photo wall frame

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'au-trip-photo-v13';
+const CACHE_NAME = 'au-trip-photo-v14';
 const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
