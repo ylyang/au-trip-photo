cf7279faa71cf776f2f79aa8cf0da9b24c14ddfd
Yiling Yang <csylyang@gmail.com>
2026-09-24 14:31:00 +0800

    data: record verified visa details

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'au-trip-photo-v10';
+const CACHE_NAME = 'au-trip-photo-v11';
 const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
