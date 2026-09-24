afd343fc7a8579f1fe214f84f2b5899a2d2a7127
Yiling Yang <csylyang@gmail.com>
2026-09-24 10:16:07 +0800

    data: add Great Ocean Road tour cost

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'au-trip-photo-v6';
+const CACHE_NAME = 'au-trip-photo-v7';
 const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
