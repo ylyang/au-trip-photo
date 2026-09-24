6f07934e63e6ea3f1aaaee420d9432b81ae6e8c9
Yiling Yang <csylyang@gmail.com>
2026-09-24 09:09:20 +0800

    feat: add navigation choice panel

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'au-trip-photo-v3';
+const CACHE_NAME = 'au-trip-photo-v4';
 const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
