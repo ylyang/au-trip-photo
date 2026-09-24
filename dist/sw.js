b53ede15d3b3950b8f2f6bfe4f34d779e9e67fb0
Yiling Yang <csylyang@gmail.com>
2026-09-24 10:20:17 +0800

    data: update Great Ocean Road pickup details

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'au-trip-photo-v7';
+const CACHE_NAME = 'au-trip-photo-v8';
 const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
