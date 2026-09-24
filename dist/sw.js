d8f19864e40fe5ed85e81420893db5524cf9457e
Yiling Yang <csylyang@gmail.com>
2026-09-24 10:59:31 +0800

    feat: add trip overview flow to itinerary

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'au-trip-photo-v8';
+const CACHE_NAME = 'au-trip-photo-v9';
 const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
