20d4710e852b891459a6d341f6f89e93cbd113a6
Yiling Yang <csylyang@gmail.com>
2026-09-24 14:26:55 +0800

    data: update travel readiness and opera conversion

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'au-trip-photo-v9';
+const CACHE_NAME = 'au-trip-photo-v10';
 const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
