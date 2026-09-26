1a3c562bc9322c6c9a7a3c054d9e7e9e574311b4
Yiling Yang <csylyang@gmail.com>
2026-09-26 20:04:13 +0800

    refine: keep D2 shopping in central Melbourne

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'au-trip-photo-v20';
+const CACHE_NAME = 'au-trip-photo-v21';
 const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
