074feac47a13693050aba02d88f983887b42e003
Yiling Yang <csylyang@gmail.com>
2026-09-26 19:58:49 +0800

    fix: use native Xiaohongshu links and add D2 shopping route

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'au-trip-photo-v19';
+const CACHE_NAME = 'au-trip-photo-v20';
 const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
