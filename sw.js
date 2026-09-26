c0d96eda969a341c38359edf406d1df6042783dd
Yiling Yang <csylyang@gmail.com>
2026-09-26 20:34:30 +0800

    feat: add D2 restaurant recommendations

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'au-trip-photo-v21';
+const CACHE_NAME = 'au-trip-photo-v22';
 const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
