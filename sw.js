53678aa8e50e74d37c4ae6b8fc9cd2b90dd1c020
Yiling Yang <csylyang@gmail.com>
2026-09-26 19:17:54 +0800

    fix: bypass Xiaohongshu login-bound search links

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'au-trip-photo-v17';
+const CACHE_NAME = 'au-trip-photo-v18';
 const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
