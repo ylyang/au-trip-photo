478a8e1218fcc8b7d9c12417661b8c4c73e6f80e
Yiling Yang <csylyang@gmail.com>
2026-09-26 22:04:42 +0800

    feat: add meal recommendations across itinerary

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'au-trip-photo-v24';
+const CACHE_NAME = 'au-trip-photo-v25';
 const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
