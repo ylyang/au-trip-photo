a6bcea8cc3d1d989c7b4e75b25b196c7f45ff90e
Yiling Yang <csylyang@gmail.com>
2026-09-26 19:27:15 +0800

    fix: open photo searches directly in Xiaohongshu

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'au-trip-photo-v18';
+const CACHE_NAME = 'au-trip-photo-v19';
 const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
